846ee668ad2d2319ca2bc5071e7d372f83e7aeab
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-11-29 16:22:49 -0500

    added js for interval and wrapping

---
+++ assignment-2/js/custom.js
@@ -0,0 +1,8 @@
+//Custom JS for Assignment 2
+const carouselBooks = document.querySelector("#trilogy");
+
+const carousel = new bootstrap.Carousel(carouselBooks, {
+    interval: 1600,
+    wrap: true,
+});
+
