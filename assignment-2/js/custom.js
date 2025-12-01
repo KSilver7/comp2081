efe389344a268901a97ffe9e4a4cba04bbc3a75b
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-11-30 19:42:46 -0500

    added/fixed the tabs/fixed naming on the tabs

--- assignment-2/js/custom.js
+++ assignment-2/js/custom.js
@@ -1,4 +1,5 @@
 //Custom JS for Assignment 2
+// carousel JS
 const carouselBooks = document.querySelector("#trilogy");
 
 const carousel = new bootstrap.Carousel(carouselBooks, {
@@ -6,3 +7,12 @@ const carousel = new bootstrap.Carousel(carouselBooks, {
     wrap: true,
 });
 
+// Tab JS (from the bootstrap document site)
+const triggerTabList = document.querySelectorAll('#sectionTab button')
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
